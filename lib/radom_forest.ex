81cf87484a92c377601997192c439598f9bc576f
Grégoire <gregoire.riviere@kbrw.fr>
2021-05-22 21:59:00 +0200

    Small changes + prepare for trading backtest

--- lib/radom_forest.ex
+++ lib/radom_forest.ex
@@ -20,21 +20,30 @@ defmodule RandomForest do
   require Logger
 
   def build_forest(dataset, criteria_list, data_sample_size, crit_number, tree_number) do
-    1..tree_number |> Enum.map(fn _ ->
+    1..tree_number |> Task.async_stream(fn _ ->
       sample = 1..data_sample_size |> Enum.map(fn _ ->
         Enum.take_random(dataset, 1)
       end) |> List.flatten
       DecisionTree.build_decision_tree(sample, Enum.take_random(criteria_list, crit_number))
-    end)
+    end, max_concurrency: 30, timeout: :infinity) |> Enum.map(fn {:ok, v} -> v end)
   end
 
-  def find_value(forest, record) do
+  # def find_value(forest, record) do
+  #   vote = forest |> Enum.map(fn tree ->
+  #     {:ok, r} = DecisionTree.find_value(tree, record)
+  #     r
+  #   end)
+  #   # Logger.debug("#{inspect vote}")
+  #   if Enum.count(vote, & &1 == :yes) > Enum.count(vote, & &1 == :no), do: :yes, else: :no
+  # end
+
+  def find_value(forest, record, needed_accuracy \\ 0.5) do
     vote = forest |> Enum.map(fn tree ->
       {:ok, r} = DecisionTree.find_value(tree, record)
       r
     end)
     # Logger.debug("#{inspect vote}")
-    if Enum.count(vote, & &1 == :yes) > Enum.count(vote, & &1 == :no), do: :yes, else: :no
+    if Enum.count(vote, & &1 == :yes) > needed_accuracy * Enum.count(vote), do: :yes, else: :no
   end
 
 end
--- test/dataset.ex
+++ test/dataset.ex
@@ -1,10 +1,11 @@
 dataset = File.read!("test/heart.csv") |> String.split("\r\n") |> Enum.reject(& &1 == "") |> tl() |> Enum.map(fn line ->
-  [age, sex, cp, _, _, fbs, _, _, exang, _, slope, ca, _, outcome] = String.split(line, ",")
+  [age, sex, cp, _, _, fbs, _, max_heart_rate, exang, _, slope, ca, _, outcome] = String.split(line, ",")
   %{
     age: age |> Integer.parse |> elem(0),
     sex: sex,
     cp: cp,
     fbs: fbs,
+    max_heart_rate: max_heart_rate |> Integer.parse |> elem(0),
     exang: exang,
     slope: slope,
     ca: ca,
@@ -12,7 +13,7 @@ dataset = File.read!("test/heart.csv") |> String.split("\r\n") |> Enum.reject(& 
   }
 end)
 
-continuous_fields = [:age]
+continuous_fields = [:age, :max_heart_rate]
 criteria_list = hd(dataset) |> Enum.map(fn {k, v} ->
   %{
     name: k,
@@ -29,7 +30,7 @@ end) |> Enum.reject (& &1.name == :outcome)
 
 {training_set, test_set} = Enum.shuffle(dataset) |> Enum.split(200)
 
-random_forest = RandomForest.build_forest(training_set, criteria_list, 100, 6, 75)
+random_forest = RandomForest.build_forest(training_set, criteria_list, 50, 4, 1000)
 
 ok_record = Enum.map(test_set, fn rec ->
   response = RandomForest.find_value(random_forest, rec)
@@ -51,3 +52,65 @@ RandomForest.find_value(random_forest, record)
 #     end)
 #   end)
 # end)
+
+file_name="new_model_cad_chf.bert"
+
+dataset = File.read!("test/#{file_name}") |> :erlang.binary_to_term |> Enum.map(fn p ->
+  %{
+    ema_position: p.ema_200 > p.close && :below || :above,
+    cloud_position: cond do
+      p.ssa >= p.close && p.ssb <= p.close -> :inside
+      p.ssa <= p.close && p.ssb >= p.close -> :inside
+      p.ssa <= p.close && p.ssb <= p.close -> :above
+      p.ssa >= p.close && p.ssb >= p.close -> :below
+    end,
+    cloud_color: p.ssa >= p.ssb && :green || :red,
+    size_atr: abs(p.close - p.open)/p.atr,
+    extension_200: abs(p.close - p.ema_200)/p.atr,
+    cross_kj: cond do
+      p.close >= p.kj && p.open <= p.kj -> :bullish
+      p.close <= p.kj && p.open >= p.kj -> :bearish
+      true -> :none
+    end
+  } |> Map.merge(p)
+end) |> Enum.map(fn p ->
+  Map.take(p, [:ts, :atr, :outcome, :rsi, :ema_position, :schaff_tc, :cloud_color, :cloud_position, :candle_color, :ema_200_pr_50, :kj_tk, :cross_kj])
+end)
+
+reject_field = [:atr, :ts]
+continuous_fields = [:rsi, :schaff_tc, :size_atr, :extension]
+criteria_list = hd(dataset) |> Enum.reject(fn {k, _} -> k in reject_field end) |> Enum.map(fn {k, _} ->
+  %{
+    name: k,
+    type:
+    (if k in continuous_fields do
+      :continuous
+    else :discrete end),
+    possible_values:
+    (if k in continuous_fields do
+      []
+    else (Enum.map(dataset, & &1[k]) |> Enum.uniq) end)
+  }
+end) |> Enum.reject (& &1.name == :outcome)
+
+# dataset = Enum.drop(dataset, 205)
+{training_set, test_set} = Enum.shuffle(dataset) |> Enum.split(2000)
+{val_set, test_set} = Enum.shuffle(test_set) |> Enum.split(2000)
+random_forest = RandomForest.build_forest(training_set, criteria_list, 750, 4, 1500)
+
+1..200 |> Enum.map(& &1/2)|> Enum.map(fn i ->
+  trade_taken = Enum.map(val_set, fn rec ->
+    response = RandomForest.find_value(random_forest, rec, i/100)
+    {response, rec.outcome}
+  end) |> Enum.filter(fn {a, _} -> a == :yes end)
+  length(trade_taken)
+  accuracy = length(trade_taken) != 0 && (trade_taken |> Enum.count(fn {_, b} -> b == :yes end)) / length(trade_taken) || 0
+  IO.puts("Nb trade taken : #{length(trade_taken)}")
+  IO.puts("Accuracy : #{accuracy}")
+  IO.puts("i : #{i/100}")
+end)
+
+trade_taken = Enum.map(test_set, fn rec ->
+  response = RandomForest.find_value(random_forest, rec, 0.2)
+  {response, rec.outcome}
+end) |> Enum.filter(fn {a, _} -> a == :yes end)
